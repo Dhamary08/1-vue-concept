76138ae09c92b0f3505506c589ab870f657b5387
Valeria Manriquez <vmanriquez@sourcing.cl>
2022-08-27 23:49:07 -0400

    Contador

---
+++ app.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Contador - Valeria Manríquez',
+    numValue: 0,
+    numQuantity: 1,
+    listHistory: [],
+    question: 'Ingrese la cantidad deseada para contar',
+    historyFinal: 'Últimos 5 resultados'
+  },
+  methods: {
+    lastFiveValue(value) {
+      this.listHistory.push(value)
+      if (this.listHistory.length > 5) {
+        this.listHistory.shift()
+      }
+    },
+    countPlus() {
+      this.numValue = (+this.numValue) + (+this.numQuantity);
+      this.lastFiveValue(this.numValue)
+      return this.numValue
+    },
+    countMinus() {
+      this.numValue = (+this.numValue) - (+this.numQuantity);
+      this.lastFiveValue(this.numValue)
+      return this.numValue
+    },
+    resetAll() {
+      this.numValue = 0,
+      this.numQuantity = 1,
+      this.listHistory=[]
+    },
+  },
+  computed: {
+    historyText() { return this.listHistory.reverse().join(`\n`)},
+  }
+})
